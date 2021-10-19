cb0c32bbb12c1a38da5d6b22370099c689b93c87
mrirasel <rasel1406@gmail.com>
2021-10-19 14:14:43 +0600

    admin/exam.php,final.php and start-exam.php,student-login.php.Classes/All.php,Exam.php.Akahne add-exam.php page new akta view button add koreci krn oi button a click krle oi id sb quetion asbe and xm dite parbe .ata just demo ai kaj hbe admin er baire

--- batch.php
+++ batch.php
@@ -13,11 +13,7 @@
     <a class="flex items-center justify-center font-semibold text-primary-500 rounded-lg border-2 text-lg h-14 w-60 border-primary transition duration-300 ease-in-out hover:bg-primary-500 hover:text-primary-50" href="class.php?cls=<?=$raw['id'];?>"><?=$raw['branch_name'];?> 
 	</a>
     <?php }}?>
-    <a class="flex items-center justify-center font-semibold text-primary-500 rounded-lg border-2 text-lg h-14 w-60 border-primary transition duration-300 ease-in-out hover:bg-primary-500 hover:text-primary-50" href="/exams/warriors">Warriors
-	</a><a class="flex items-center justify-center font-semibold text-primary-500 rounded-lg border-2 text-lg h-14 w-60 border-primary transition duration-300 ease-in-out hover:bg-primary-500 hover:text-primary-50" href="/exams/prottasa">প্রত্যাশা  
-	</a><a class="flex items-center justify-center font-semibold text-primary-500 rounded-lg border-2 text-lg h-14 w-60 border-primary transition duration-300 ease-in-out hover:bg-primary-500 hover:text-primary-50" href="/exams/gke-crash-course">GKE Crash Course 
-	</a><a class="flex items-center justify-center font-semibold text-primary-500 rounded-lg border-2 text-lg h-14 w-60 border-primary transition duration-300 ease-in-out hover:bg-primary-500 hover:text-primary-50" href="/exams/and">অনুশীলনী &amp; বিগত বছরের  প্রশ্ন 
-	</a></div></section></div>
+    </div></section></div>
 	<footer class="text-gray-600 body-font"><div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col"><p class="text-sm text-gray-500_ sm:py-2 sm:mt-0 mt-4">© 2021 MediExam Doctor —
 	<a href="/about" class="text-gray-600 ml-1">Hasebul Hasan Santo</a></p>
 <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"><a class="text-gray-500" href="/"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16"><path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path></svg></a>
--- class.php
+++ class.php
@@ -7,89 +7,96 @@
 <?php
    if(isset($_GET['cls'])){
     $subid = $_GET['cls'];
-  
+    $sub = $common->select("`subject_add`","`batch_id`='$subid'");
 }
+
 ?>
+
 <DOCTYPE html>
   <html>
     <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  
+    <meta charset="utf-8" />
     </head>
     <body>
       <div class="container">
         <img src="image/04.jpg" class="rounded mx-auto d-block"  width="75%">
 
-        <div class="sub mt-3">
+        <ul class="nav nav-pills mb-3 mt-5" id="pills-tab" role="tablist">
           <?php
+            if($sub){
+              $sl = '1';
+              while($raw = mysqli_fetch_assoc($sub)){
+              ?>
+              <li class="nav-item" role="presentation">
+                <button class="nav-link<?= $sl == 1 ? ' active' : ''; ?>" id="pills-s<?= $sl; ?>-tab" data-bs-toggle="pill" data-bs-target="#pills-s<?= $sl; ?>" type="button" role="tab" aria-controls="pills-s<?= $sl; ?>" aria-selected="<?= $sl == 1 ? 'true' : 'false'; ?>"><?=$raw['subject_name'];?></button>
+              </li>
+              <?php
+              $sl++;
+              }
+            }
+          ?>
+        </ul>
+
+        <div class="tab-content" id="pills-tabContent">
+            <?php
             $sub = $common->select("`subject_add`","`batch_id`='$subid'");
             if($sub){
+              $csl = '1';
               while($raw = mysqli_fetch_assoc($sub)){
+              ?>
+              <div class="tab-pane fade<?= $csl == 1 ? ' show active' : ''; ?>" id="pills-s<?= $csl; ?>" role="tabpanel" aria-labelledby="pills-s<?= $csl; ?>-tab">
+              <table class="table table-hover mt-3">
+                <tr class="table-info">
+                  <th class="">অধ্যায়</th>
+                  <th class="">টপিক</th>
+                  <th class="">ক্লাস</th>
+                  <th class="">পরীক্ষা</th>
+                  <th class="">নোট</th>
+                </tr>
+                <?php
               
-          ?>
-          <button type="button" class="btn btn-outline-primary"><?=$raw['subject_name'];?></button>
-         <?php }}?>
+              $tid = $raw['id'];
+              $topics = $common->select("`class_add`","`subject_id`='$tid' ORDER BY `chapter` ASC");
+                  if($topics){
+                    while($rows = mysqli_fetch_assoc($topics)){
+                  
+                ?>
+                <tr>
+                  <td>
+                    <?=$rows['chapter'];?>
+                  </td>
+                  <td>
+                  <?=$rows['topic'];?>
+                  </td>
+                  <td>
+                    <a href="">
+                      <img src="image/lecture.png" height="30px;">
+                    </a>
+                  </td>
+                  <td>
+                    <a href="student-login.php">
+                      <img src="image/exam.png" height="30px;">
+                    </a>
+                  </td>
+                  <td>
+                    <a href="">
+                      <img src="image/note.png" height="30px;">
+                    </a>
+                  </td>
+                </tr>
+                <?php }}?>
+              </table>
+              </div>
+              <?php
+              $csl++;
+              }
+            }
+            ?>
         </div>
-
-        <table class="table table-hover mt-3">
-          <tr class="table-info">
-            <th class="">অধ্যায়</th>
-            <th class="">টপিক</th>
-            <th class="">ক্লাস</th>
-            <th class="">পরীক্ষা</th>
-            <th class="">নোট</th>
-          </tr>
-          <tr>
-            <td>
-              All
-            </td>
-            <td>
-              All is good
-            </td>
-            <td>
-              <a href="">
-                <img src="image/lecture.png" height="30px;">
-              </a>
-            </td>
-            <td>
-              <a href="">
-                <img src="image/exam.png" height="30px;">
-              </a>
-            </td>
-            <td>
-              <a href="">
-                <img src="image/note.png" height="30px;">
-              </a>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              All
-            </td>
-            <td>
-              All is good
-            </td>
-            <td>
-              <a href="">
-                <img src="image/lecture.png" height="30px;">
-              </a>
-            </td>
-            <td>
-              <a href="">
-                <img src="image/exam.png" height="30px;">
-              </a>
-            </td>
-            <td>
-              <a href="">
-                <img src="image/note.png" height="30px;">
-              </a>
-            </td>
-          </tr>
-        </table>
       </div>
       
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"   crossorigin="anonymous"></script>
-          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
