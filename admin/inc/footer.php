276bc15c418f4ca150a0aa56e4e8aa8d0263b104
ARIF <arifh3261@gmail.com>
2021-11-30 21:18:50 +0600

    Everything deleted

--- admin/inc/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="footer">
-    All Rights Reserved by Materialpro admin.
-</footer>
--- css/app/client.js
+++
@@ -1,1 +0,0 @@
-import{U as o}from"./vendor-d2462958.js";var r=o({projectId:"e5mb4tjm",dataset:"production",apiVersion:"2021-08-13",useCdn:!1});export{r as c};
