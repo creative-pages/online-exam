c41dedcce16ac9e0332effb79de5a7a46ca1c013
ARIF <arifh3261@gmail.com>
2022-01-08 23:23:40 +0600

    student batch.php page update

--- student/batch.php
+++ student/batch.php
@@ -46,6 +46,13 @@
                     $batch_info = $common->select("`add_branch`", "`id` = '$batch_id'");
                     $batch_infos = mysqli_fetch_assoc($batch_info);
 
+                    // exam info
+                    $exam_info = $common->select("`add_exam`", "`batch_id` = '$batch_id'");
+                    $exam_infos = mysqli_fetch_assoc($exam_info);
+                    // result info
+                    $exam_id = $exam_infos['id'];
+                    $result_info = $common->select("`results`", "`exam_id` = '$exam_id' && `student_id` = '$pid'");
+
                     if ($batch_infos['type'] == 'paid') {
                         // payment request pending
                         $check_payment_request = $common->select("`pay_requests`", "`user_id` = '$pid' && `batch_id` = '$batch_id' && `status` = '0'");
@@ -194,17 +201,32 @@
                                         <!-- Modal end -->
                                     </td>
                                 </tr>
-
+                                <?php
+                                }
+                                ?>
+                                <?php
+                                if($result_info) {
+                                ?>
                                 <tr>
-                                    <td>
-                                    <p>
-                                        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                            Button with data-bs-target
-                                        </button>
+                                    <td colspan="4">
+                                        <p class="mb-0">
+                                          <button class="btn btn-primary w-100" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                            Exam Results
+                                          </button>
                                         </p>
                                         <div class="collapse" id="collapseExample">
-                                        <div class="card card-body">
-                                            Some placeholder content for the
+                                          <div class="card card-body p-0 mb-0">
+                                          <?php
+                                          $i = 1;
+                                          while ($result_row = mysqli_fetch_assoc($result_info)) {
+                                          ?>
+                                          <a class="btn btn-outline-info w-100 mt-1" href="result.php?result_id=<?= $result_row['id']; ?>">Exam <?= $i . ' &nbsp;&nbsp;&nbsp;-&nbsp;&nbsp;&nbsp; ' . date("d-M-Y", strtotime($result_row['add_time'])); ?></a>
+                                          <?php
+                                          $i++;
+                                          }
+                                          ?>
+                                          </div>
+                                        </div>
                                     </td>
                                 </tr>
                                 <?php
@@ -239,7 +261,7 @@
                     }
                 }
             } else {
-
+                echo '<h4 class="text-center mt-4 text-danger">You have no batch!</h4>';
             }
             ?>
         </div>
