2c6bc8103cfeda1bbe855db2276bb0a085cef8a0
mrirasel <rasel1406@gmail.com>
2022-01-10 23:07:09 +0600

    hello

--- student/batch.php
+++ student/batch.php
@@ -194,17 +194,22 @@
                                         <!-- Modal end -->
                                     </td>
                                 </tr>
+                                <?php
+                                
+                                ?>
 
                                 <tr>
                                     <td>
-                                    <p>
-                                        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                            Button with data-bs-target
-                                        </button>
+                                        <p>
+                                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                                Exam Result
+                                            </button>
                                         </p>
                                         <div class="collapse" id="collapseExample">
                                         <div class="card card-body">
-                                            Some placeholder content for the
+                                            Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
+                                        </div>
+                                        </div>
                                     </td>
                                 </tr>
                                 <?php
